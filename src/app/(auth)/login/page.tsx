51e6e93ddae7b7e7fb128334945d6bff49c2637c
Raka <incudelta@gmail.com>
2025-08-14 11:31:30 +0700

    modify: change color link register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -81,7 +81,7 @@ export default function MasukPage() {
 
         <div className="mt-4 text-center text-sm">
           Belum punya akun?{" "}
-          <Link href="/register" className="text-primary hover:underline">
+          <Link href="/register" className="text-blue-600 hover:underline">
             Daftar di sini
           </Link>
         </div>
