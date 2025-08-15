b27232311ef3823bbc4e11d088d3a39d0be68992
Raka <incudelta@gmail.com>
2025-08-15 11:32:00 +0700

    modify: remove bg color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -62,5 +62,5 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     )
   }
 
-  return <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">{children}</div>
+  return <div className="min-h-screen flex items-center justify-center px-4">{children}</div>
 }
