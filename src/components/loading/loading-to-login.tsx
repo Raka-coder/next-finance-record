7e85b30970dabdab539ca08f434b8189746cb39d
Raka <incudelta@gmail.com>
2025-08-18 18:41:11 +0700

    create: loading to login component

---
+++ src/components/loading/loading-to-login.tsx
@@ -0,0 +1,9 @@
+export default function LoadingToLogin() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <p className="text-gray-600">Redirecting to login...</p>
+      </div>
+    </div>
+  );
+}
