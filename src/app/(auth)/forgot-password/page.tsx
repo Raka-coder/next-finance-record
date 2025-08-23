4acd2673b4cf7ca7bae4af1e7635e8b065d2fe77
Raka <incudelta@gmail.com>
2025-08-23 23:49:02 +0700

    add: create Forgot Password and Update Password pages with respective forms

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import { ForgotPasswordForm } from "@/components/auth-component/forgot-password/forgot-password-form"
+
+export default function page() {
+  return (
+    <div>
+        <ForgotPasswordForm />
+    </div>
+  )
+}
---
+++ src/app/(auth)/update-password/page.tsx
@@ -0,0 +1,9 @@
+import { UpdatePasswordForm } from "@/components/auth-component/update-password/update-password-form"
+
+export default function page() {
+  return (
+    <div>
+        <UpdatePasswordForm />
+    </div>
+  )
+}
