6d4000dd04ffc4ea41401133dc548f0c9c2bd020
Raka <incudelta@gmail.com>
2025-08-31 13:31:05 +0700

    refactor: update title page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import LandingPage from "@/components/landing/landing-page";
 import { generateMetadata } from "@/lib/metadata";
 
 export const metadata = generateMetadata({
-  title: "Beranda",
   description: "Aplikasi pengelolaan keuangan pribadi yang membantu Anda melacak pemasukan, pengeluaran, dan mencapai tujuan finansial.",
 });
 
